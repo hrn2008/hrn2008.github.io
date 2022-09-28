95d41ea8833a7ec5391885b4b86ca6c18de4c2f9
hrn2008 <hrn2008@yahoo.com>
2022-09-28 02:46:55 -0400

    about me page

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,24 @@
+"use strict";
+//IIFE
 (function()
 {
+
     function Start()
     {
         console.log("App Started!")
     }
     window.addEventListener("load", Start);
 })();
+
+
+
+// https://www.youtube.com/watch?v=JkuiKeNS2mg
+const activePage=window.location.pathname;
+// console.log(activePage);
+const navlinks=document.querySelectorAll('.nav a').forEach(link => {
+    // console.log(link.href);
+    if (link.href.includes(`${activePage}`)){
+        // console.log(`${activePage}`);
+        link.classList.add('active');
+    }
+})
