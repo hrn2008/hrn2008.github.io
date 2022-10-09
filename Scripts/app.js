664c9c9c126997683fbc44ed46139b0c487d2793
hrn2008 <hrn2008@yahoo.com>
2022-10-09 12:52:25 -0400

    front-end(html and css) is done

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,24 +1,46 @@
 "use strict";
 //IIFE
-(function()
-{
+// (function()
+// {
 
-    function Start()
-    {
-        console.log("App Started!")
-    }
-    window.addEventListener("load", Start);
-})();
+//     function Start()
+//     {
+//         console.log("App Started!")
+//     }
+//     window.addEventListener("load", Start);
+// })();
 
 
 
 // https://www.youtube.com/watch?v=JkuiKeNS2mg
-const activePage=window.location.pathname;
+let activePage = window.location.pathname;
 // console.log(activePage);
-const navlinks=document.querySelectorAll('.nav a').forEach(link => {
+if (activePage == "/") {
+    activePage = "/index.html";
+    // console.log("sdfdfd-");
+    // console.log(activePage);
+} else {
+    activePage = window.location.pathname;
+}
+console.log(activePage);
+
+
+// const spanLinks = document.querySelectorAll('ul li span');
+
+// console.log(`${activePage}`)
+// spanLinks.forEach(span=>{
+
+//     if(`${span.id}.html` == (`${activePage}`)){
+//         span.classList.add("selectedOption")
+//     }
+// })
+
+
+const navlinks = document.querySelectorAll('.nav a').forEach(link => {
     // console.log(link.href);
-    if (link.href.includes(`${activePage}`)){
+    if (link.href.includes(`${activePage}`)) {
         // console.log(`${activePage}`);
         link.classList.add('active');
     }
 })
+
